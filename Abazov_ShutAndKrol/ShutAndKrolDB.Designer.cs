eff74e32d1e260f37ffb9b2a21b3bac6065a9de6
235002 <235002@KB37-303-C04>
2026-05-21 14:00:52 +0300

    уп окончена

--- Abazov_ShutAndKrol/ShutAndKrolDB.Designer.cs
+++ Abazov_ShutAndKrol/ShutAndKrolDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\235002\source\repos\Abazov_ShutAndKrol\Abazov_ShutAndKrol\ShutAndKrolDB.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\235002\source\repos\ABAZOV1087\Abazov_ShutAndKrol\Abazov_ShutAndKrol\ShutAndKrolDB.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
